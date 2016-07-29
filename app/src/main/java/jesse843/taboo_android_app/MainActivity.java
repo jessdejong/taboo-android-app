933e47c38b8d1b65d9611deb61944a774ff27610
Jesse DeJong <jessedejong101@gmail.com>
2016-07-29 13:56:05 -0500

    Animation

--- app/src/main/java/jesse843/taboo_android_app/MainActivity.java
+++ app/src/main/java/jesse843/taboo_android_app/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +17,8 @@ public class MainActivity extends AppCompatActivity {
     public void settingsOnClick (View v) {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
+    }
+
+    public void informationOnClick (View v) {
     }
 }
