7baf4a1cf97ed9af9120ba4aefe9a3cbd600cd70
Jesse DeJong <jessedejong101@gmail.com>
2016-07-29 13:13:36 -0500

    Adding Settings Activity

--- app/src/main/java/jesse843/taboo_android_app/MainActivity.java
+++ app/src/main/java/jesse843/taboo_android_app/MainActivity.java
@@ -1,7 +1,9 @@
 package jesse843.taboo_android_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void settingsOnClick (View v) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
