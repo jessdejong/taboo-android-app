375808651b9df6cdbda6a2c86916feb0a4857f4b
Jesse DeJong <jessedejong101@gmail.com>
2016-09-03 22:04:45 -0500

    Adding play button to GameSettings activity

--- app/src/main/java/jesse843/taboo_android_app/GameSettingsActivity.java
+++ app/src/main/java/jesse843/taboo_android_app/GameSettingsActivity.java
@@ -1,22 +1,14 @@
 package jesse843.taboo_android_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class GameSettingsActivity extends AppCompatActivity {
-    // num rounds SeekBar
-    private SeekBar numRoundsSeekBar;
-
-    // num rounds choose TextView
-    private TextView numRoundsTextView;
-
-    // time in round SeekBar
-    private SeekBar timeInRoundSeekBar;
-
-    // time in round TextView
-    private TextView timeInRoundTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +16,13 @@ public class GameSettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game_settings);
 
         // initializing variables
-        numRoundsSeekBar = (SeekBar)findViewById(R.id.num_rounds_seek_bar);
-        numRoundsTextView = (TextView)findViewById(R.id.num_rounds_choose_text_view);
-        timeInRoundSeekBar = (SeekBar)findViewById(R.id.time_in_round_seek_bar);
-        timeInRoundTextView = (TextView)findViewById(R.id.time_in_round_choose_text_view);
+        SeekBar numRoundsSeekBar = (SeekBar)findViewById(R.id.num_rounds_seek_bar);
+        final TextView numRoundsTextView = (TextView)findViewById(R.id.num_rounds_choose_text_view);
+        SeekBar timeInRoundSeekBar = (SeekBar)findViewById(R.id.time_in_round_seek_bar);
+        final TextView timeInRoundTextView = (TextView)findViewById(R.id.time_in_round_choose_text_view);
+        Button play_button = (Button)findViewById(R.id.to_game_button);
+
+        assert play_button != null;
 
         numRoundsSeekBar.setProgress(5);
         numRoundsTextView.setText(getResources().getString(R.string.num_rounds_choose_text) + " " + numRoundsSeekBar.getProgress());
@@ -35,6 +30,7 @@ public class GameSettingsActivity extends AppCompatActivity {
         timeInRoundSeekBar.setProgress(30);
         timeInRoundTextView.setText(getResources().getString(R.string.time_in_round_choose_text) + " " + timeInRoundSeekBar.getProgress());
 
+        // numRoundsSeekBar setOnSeekBarChangeListener
         numRoundsSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -48,6 +44,7 @@ public class GameSettingsActivity extends AppCompatActivity {
             }
         });
 
+        // timeInRoundSeekBar setOnSeekBarChangeListener
         timeInRoundSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -58,6 +55,15 @@ public class GameSettingsActivity extends AppCompatActivity {
             }
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        // play button clicked
+        play_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GameSettingsActivity.this, GameActivity.class);
+                startActivity(intent);
             }
         });
     }
