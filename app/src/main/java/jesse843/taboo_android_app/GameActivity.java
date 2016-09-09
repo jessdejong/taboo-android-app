36a896fc21c9f4b3f4408bf124ea1d78027e5a9c
Jesse DeJong <jessedejong101@gmail.com>
2016-09-08 21:24:25 -0500

    Getting team names in GameActivity

--- app/src/main/java/jesse843/taboo_android_app/GameActivity.java
+++ app/src/main/java/jesse843/taboo_android_app/GameActivity.java
@@ -1,13 +1,20 @@
 package jesse843.taboo_android_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class GameActivity extends AppCompatActivity {
+    private String team_name1;
+    private String team_name2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        Intent intent = getIntent();
+        String team_name1 = intent.getStringExtra(GameSettingsActivity.TEAM_ONE_NAME);
+        String team_name2 = intent.getStringExtra(GameSettingsActivity.TEAM_TWO_NAME);
     }
 }
