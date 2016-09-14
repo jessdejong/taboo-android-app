9392af8993d1a35484267e20b082f07c50e8b864
Jesse DeJong <jessedejong101@gmail.com>
2016-09-13 22:07:21 -0500

    Added comment

--- app/src/main/java/jesse843/taboo_android_app/GameActivity.java
+++ app/src/main/java/jesse843/taboo_android_app/GameActivity.java
@@ -23,6 +23,7 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        // gets intent
         Intent intent = getIntent();
 
         // Initializing Variables
